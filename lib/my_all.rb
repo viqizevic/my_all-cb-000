bef31444dee10c545e4f1b3740d0f7e4f1b64f1a
Vicky Tanzil <vicky.tanzil@gmail.com>
2018-09-19 22:14:04 +0200

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all?(array)
+  # code here
+  i = 0
+  while i < array.size
+    return false if not yield(array[i])
+    i += 1
+  end
+  true
 end
